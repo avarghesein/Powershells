0ecaff81d86b78dc2857f224be6a8162e664814c
avarghesein <abraham.varghese@hotmail.com>
2019-01-07 22:48:57 +0530

    ?: samples updated

--- VAPowerShells/Samples/VA.Script/VA.Script.Test_Common.ps1
+++ VAPowerShells/Samples/VA.Script/VA.Script.Test_Common.ps1
@@ -1,18 +1,23 @@
  Clear
-
+ #
  Import-Module -Name "VA.Script.Utility" -Force
-
+ #
  #Usage condition |?: truepart falsePart or condition |?: truePartOnly
  $true |?: "Print True" "Print False"
- 
+ #
+ #
  #Usage {condition} |?: {trueBlock} {falseBlock} or {condition} |?: {trueBlockOnly}
  { $true } |?: { "Print True" } { "Print False" }
-
+ #
+ #
  #You could provide condition directly without script block
  1 -eq 2  |?: { "1 Equals 2" } { "1 Not Equals 2" }
-
+ #
+ #
  #You could omit False Part of the ternary
  { 1 -eq 2 } |?: { "1 Equals 2" }
-
- #Use Ternary in Pipe line
+ #
+ #
+ #Use Ternary in Pipe line. Below will Print 3
  (@( {$true}, $true, { $false }, $true) | ?: "YES" ).Length
+ #
