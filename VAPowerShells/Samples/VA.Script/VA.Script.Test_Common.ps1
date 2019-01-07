9f75253afe7d26e948c43e0a350366d31bc3ba57
Abraham Varghese <10294917+avarghesein@users.noreply.github.com>
2019-01-07 10:58:39 +0530

    ?: non-script block support

--- VAPowerShells/Samples/VA.Script/VA.Script.Test_Common.ps1
+++ VAPowerShells/Samples/VA.Script/VA.Script.Test_Common.ps1
@@ -2,8 +2,11 @@
 
  Import-Module -Name "VA.Script.Utility" -Force
 
- #Usage {condition} |?: {trueblock} {falseblock} or {condition} |?: {trueblock}
- { $true } |?: { "True" } { "False" }
+ #Usage condition |?: truepart falsePart or condition |?: truePartOnly
+ $true |?: "Print True" "Print False"
+ 
+ #Usage {condition} |?: {trueBlock} {falseBlock} or {condition} |?: {trueBlockOnly}
+ { $true } |?: { "Print True" } { "Print False" }
 
  #You could provide condition directly without script block
  1 -eq 2  |?: { "1 Equals 2" } { "1 Not Equals 2" }
@@ -12,4 +15,4 @@
  { 1 -eq 2 } |?: { "1 Equals 2" }
 
  #Use Ternary in Pipe line
- (@( {$true}, $true, { $false }, $true) | ?: { "YES" } ).Length
+ (@( {$true}, $true, { $false }, $true) | ?: "YES" ).Length
